3d94287342164144de12dff0dd33ce8f7fe79287
Alastair <alastair@alastairanderson.com>
2019-06-16 09:11:46 +0100

    Added script to access quandl API

---
+++ quandl/main.py
@@ -0,0 +1,19 @@
+# https://github.com/quandl/quandl-python
+# https://pypi.org/project/Quandl/
+
+
+import quandl
+
+
+# TODO: Ensure these are stored in environment variables before checking this in.
+API_KEY = 'ryKLxaSSXe41u8EFoVnh'
+
+
+# 1. Authenticate
+quandl.ApiConfig.api_key = API_KEY
+
+# Example: Download data for Nokia
+data = quandl.get_table('MER/F1', compnumber="39102", paginate=True)
+
+# Notes:
+#  1. type(data) = pandas DataFrame
