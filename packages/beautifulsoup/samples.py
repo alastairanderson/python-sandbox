7a8fdd6138edf0f0371315052a6b7d970bf4441b
Alastair <alastair@alastairanderson.com>
2019-06-23 21:40:59 +0100

    More bs4 examples

--- packages/beautifulsoup/samples.py
+++ packages/beautifulsoup/samples.py
@@ -3,7 +3,6 @@ beautifulsoup4==4.7.1
 lxml==4.3.4
 
 Notes:
-- Open in the 'packages/beautifulsoup' folder and venv created in their also.
 - PyPi package 'requests' is required for retrieving HTML page with the following code
 '''
 
@@ -20,7 +19,6 @@ if os.path.isfile(LOCAL_HTML_FILE):
     raw_html = file.read()
     file.close()
 else:
-    # response = requests.get(URL, headers=headers)
     response = requests.get(URL)
 
     if response:
@@ -31,11 +29,61 @@ else:
                 f.close()
 #endregion
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup       # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
-html_content = BeautifulSoup(raw_html, "lxml")
 
-print(html_content)
+# Create a bs object from the raw html with a specified parser (lxml)
+soup = BeautifulSoup(raw_html, "lxml")   # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#beautifulsoup
+
+# Kinds of objects
+## bs4.element.Tag
+tag = soup.li
+
+## .name
+tag.name
+
+## Attributes
+### access value of an individual attribute
+tag['class']    # access value of the class attribute
+tag.attrs       # access the dictionary of the attributes
+
+### add, remove, and modify a tag’s attributes
+tag['id'] = 'verybold'
+tag['another-attribute'] = 1
+
+del tag['id']
+del tag['another-attribute']
+
+# tag['id']             # KeyError: 'id'
+tag.get('id')           # None
+
+## Multi-valued attributes
+
+
+
+# Searching the tree
+## Filters
+### a string
+list_items = soup.find_all('li')
+
+#### find_all() returns a list of bs4.element.Tag
+
+### a regex
+# --- finds all the tags whose names start with the letter “b”; in this case, the <body> tag and the <b> tag
+import re
+for tag in soup.find_all(re.compile("^b")):
+    print(tag.name)
+
+# --- finds all the tags whose names contain the letter ‘t’
+for tag in soup.find_all(re.compile("t")):
+    print(tag.name)
+
+
+
+
+# Example 1: Extract a collection of elements 
+
+
 
 # entries_identifier = {
 #     "element": "li",
