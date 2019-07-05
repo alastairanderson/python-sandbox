b446756e3bda9b5eaa7899e80b1835b0256bd5e9
Alastair <alastair@alastairanderson.com>
2019-07-05 09:07:39 +0100

    TF basic examples

---
+++ packages/tf/docs/1.x/02-nn-basic-classification.py
@@ -0,0 +1,18 @@
+'''
+https://www.tensorflow.org/tutorials/keras/basic_classification
+
+requirements.txt
+tensorflow==1.14.0
+matplotlib==3.1.1
+'''
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
