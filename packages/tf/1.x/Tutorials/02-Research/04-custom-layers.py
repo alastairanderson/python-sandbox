76c26a4ab686d120f7bd749ec86a62000c077d7e
Alastair <alastair@alastairanderson.com>
2019-07-15 21:07:10 +0100

    comment change

--- packages/tf/1.x/Tutorials/02-Research/04-custom-layers.py
+++ packages/tf/1.x/Tutorials/02-Research/04-custom-layers.py
@@ -104,7 +104,7 @@ late variable creation based on the shape of the inputs the layer will operate o
 hand, creating variables in __init__ would mean that shapes required to create the variables 
 will need to be explicitly specified.
 '''
-#endregion - Implementing custom layers
+
 class MyDenseLayer(tf.keras.layers.Layer):
     def __init__(self, num_outputs):
         super(MyDenseLayer, self).__init__()
@@ -131,3 +131,61 @@ print(layer.trainable_variables)
         array([[-0.6060415 , -0.1240592 , ... ,  0.09273005,  0.04697657]],
         dtype=float32)>]
 '''
+
+'''
+Overall code is easier to read and maintain if it uses standard layers whenever possible, 
+as other readers will be familiar with the behavior of standard layers. If you want to use 
+a layer which is not present in tf.keras.layers or tf.contrib.layers, consider filing a 
+github issue or, even better, sending us a pull request!
+
+github issues - http://github.com/tensorflow/tensorflow/issues/new
+'''
+#endregion - Implementing custom layers
+
+#region Models: composing layers
+'''
+Models: composing layers
+https://www.tensorflow.org/tutorials/eager/custom_layers#models_composing_layers
+
+Many interesting layer-like things in machine learning models are implemented by composing 
+existing layers. For example, each residual block in a resnet is a composition of convolutions, 
+batch normalizations, and a shortcut.
+
+The main class used when creating a layer-like thing which contains other layers is 
+tf.keras.Model. Implementing one is done by inheriting from tf.keras.Model.
+'''
+class ResnetIdentityBlock(tf.keras.Model):
+    def __init__(self, kernel_size, filters):
+        super(ResnetIdentityBlock, self).__init__(name='')
+        filters1, filters2, filters3 = filters
+
+        self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
+        self.bn2a = tf.keras.layers.BatchNormalization()
+
+        self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
+        self.bn2b = tf.keras.layers.BatchNormalization()
+
+        self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
+        self.bn2c = tf.keras.layers.BatchNormalization()
+
+    def call(self, input_tensor, training=False):
+        x = self.conv2a(input_tensor)
+        x = self.bn2a(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2b(x)
+        x = self.bn2b(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2c(x)
+        x = self.bn2c(x, training=training)
+
+        x += input_tensor
+        return tf.nn.relu(x)
+
+block = ResnetIdentityBlock(1, [1, 2, 3])
+print(block(tf.zeros([1, 2, 3, 3])))
+print([x.name for x in block.trainable_variables])
+
+
+#endregion - Models: composing layers
